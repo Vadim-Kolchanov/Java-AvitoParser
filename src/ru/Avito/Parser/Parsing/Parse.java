fbe70e204068778db64f361c2fa0d0c760bf63aa
Vadim Kolchanov <ikolchanov14@gmail.com>
2021-04-15 20:26:20 +0500

    delete unnecessary imports

--- src/ru/Avito/Parser/Parsing/Parse.java
+++ src/ru/Avito/Parser/Parsing/Parse.java
@@ -1,9 +1,6 @@
 package ru.Avito.Parser.Parsing;
 
-import ru.Avito.Parser.MyException.AllPagesHaveBeenParsingException;
-
 import java.io.IOException;
-import java.util.List;
 
 /**
  * @Project JavaParserAvito
