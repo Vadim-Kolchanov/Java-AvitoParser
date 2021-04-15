12ac1c60858f694718f9709fee8185feca426e82
Vadim Kolchanov <ikolchanov14@gmail.com>
2021-04-15 21:09:47 +0500

    adding new parse parameters

---
+++ src/ru/Avito/Parser/Parsing/ParsingApartment/ParametersApartment/TypeWalls.java
@@ -0,0 +1,66 @@
+package ru.Avito.Parser.Parsing.ParsingApartment.ParametersApartment;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import ru.Avito.Parser.Pages.Page;
+import ru.Avito.Parser.Parsing.ParsingApartment.BlockWithParameters;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Project JavaParserAvito
+ * @Author Kolchanov Vadim
+ */
+public class TypeWalls implements Page {
+
+    private final Page pageApartment;
+
+    public TypeWalls(Page pageApartment) {
+        this.pageApartment = pageApartment;
+    }
+
+    @Override
+    public Elements getElements() throws IOException {
+        return this.pageApartment.getElements();
+    }
+
+    @Override
+    public List<String> getContent() throws IOException {
+        List<String> content = this.pageApartment.getContent();
+        String typeWalls = "";
+        Element typeWallsElement = new BlockWithParameters(
+                getElements().select("li.item-params-list-item"),
+                "Тип дома"
+        ).getParameter();
+        if (typeWallsElement != null) {
+            typeWalls = typeWallsElement.text()
+                    .split(":")[1]
+                    .trim();
+            typeWalls = transformTypeWallsInNumber(typeWalls);
+        }
+        content.add(typeWalls + ";");
+        return content;
+    }
+
+    private String transformTypeWallsInNumber(String typeWalls) {
+        List<String> namesTypeWalls = new ArrayList<String>(
+                Arrays.asList(
+                        "блочный",
+                        "шлакоблочный",
+                        "деревянный",
+                        "монолитный",
+                        "газобетонный",
+                        "панельный",
+                        "кирпичный")
+        );
+        if (namesTypeWalls.contains(typeWalls)) {
+            return String.valueOf(
+                    namesTypeWalls.indexOf(typeWalls) + 1
+            );
+        }
+        return null;
+    }
+}
