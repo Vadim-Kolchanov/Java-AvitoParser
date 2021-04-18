ac7d3a0f8f1b355878d164b93f36ba0360615f8f
Vadim Kolchanov <ikolchanov14@gmail.com>
2021-04-18 15:08:11 +0500

    The function has been adding try catch

--- src/ru/Avito/Parser/Pages/Pagination.java
+++ src/ru/Avito/Parser/Pages/Pagination.java
@@ -16,12 +16,15 @@ public class Pagination {
 
     private final String urlPage;
 
-    public Pagination(String urlPage) throws IOException {
+    public Pagination(String urlPage) {
         this.urlPage = urlPage;
     }
 
-    public int checkLimit(int limitPage) throws IOException {
+    public int checkLimit(int limitPage) {
         int maxPage = getPagination();
+        if (maxPage == 0) {
+            return 0;
+        }
         while (!(limitPage > 0 && limitPage < maxPage)) {
             System.out.println("\nВведите другое число для лимита страниц");
             System.out.println("Мин. число: 1, Макс. число: " + maxPage);
@@ -32,18 +35,25 @@ public class Pagination {
         return limitPage;
     }
 
-    public int getPagination() throws IOException {
-        Elements elements = getBlockWithPageNumbers();
-        String href = elements.get(elements.size() - 1)
-                .attr("href");
-        if (href.equals("")) {
-            return 1;
+    public int getPagination() {
+        try {
+            Elements elements = getBlockWithPageNumbers();
+            String href = elements.get(elements.size() - 1)
+                    .attr("href");
+            if (href.equals("")) {
+                return 1;
+            }
+            String query = new URL("https://www.avito.ru" + href)
+                    .getQuery();
+            return Integer.parseInt(
+                    query.split("=")[1]
+            );
+        } catch (IOException ex) {
+            System.out.print("Error in getPagination: ");
+            System.out.println(ex.getMessage());
+            return 0;
         }
-        String query = new URL("https://www.avito.ru" + href)
-                .getQuery();
-        return Integer.parseInt(
-                query.split("=")[1]
-        );
+
     }
 
     public Elements getBlockWithPageNumbers() throws IOException {
