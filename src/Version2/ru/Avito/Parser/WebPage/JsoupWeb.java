b35ea18cda14b334ebdfc54f4c51a3e4fa8b74bb
Vadim Kolchanov <ikolchanov14@gmail.com>
2021-04-21 15:49:19 +0500

    Creating fake class for easy UNIT-test

--- src/Version2/ru/Avito/Parser/WebPage/JsoupWeb.java
+++ src/Version2/ru/Avito/Parser/WebPage/JsoupWeb.java
@@ -1,9 +1,13 @@
 package Version2.ru.Avito.Parser.WebPage;
 
 import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 /**
  * @Project JavaParserAvito
@@ -14,4 +18,27 @@ public interface JsoupWeb {
     Connection connect();
 
     Document parsedHTML() throws IOException;
+
+    final class Fake implements JsoupWeb {
+        private final JsoupWeb origin;
+
+        public Fake(final JsoupWeb origin) {
+            this.origin = origin;
+        }
+
+        @Override
+        public Connection connect() {
+            return this.origin.connect();
+        }
+
+        @Override
+        public Document parsedHTML() throws IOException {
+            return Jsoup.parse(
+                    new File(
+                            "D:\\Java\\JavaParserAvito\\src\\Version2\\MaterialsForFakeClass\\ForJsoupWeb\\avitoHTML-perm-kvartiry-prodam.html"
+                    ),
+                    Charset.defaultCharset().name()
+            );
+        }
+    }
 }
