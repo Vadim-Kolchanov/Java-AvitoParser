cdca872b9fb8e5b2fe09047db05412158440f799
Vadim Kolchanov <ikolchanov14@gmail.com>
2021-04-23 20:17:37 +0500

    Adding new atr

--- src/Version2/ru/Avito/Parser/WebPage/RetriedWebPage.java
+++ src/Version2/ru/Avito/Parser/WebPage/RetriedWebPage.java
@@ -13,18 +13,24 @@ public final class RetriedWebPage implements JsoupWeb {
 
     private final JsoupWeb webPage;
     private final int attempts;
+    private final SafeThreadSleep threadSleep;
 
     public RetriedWebPage(String url) {
         this(new WebPage(url));
     }
 
     public RetriedWebPage(JsoupWeb webPage) {
-        this(webPage, 5);
+        this(
+                webPage,
+                5,
+                new SafeThreadSleep(5000)
+        );
     }
 
-    private RetriedWebPage(JsoupWeb webPage, int attempts) {
+    private RetriedWebPage(JsoupWeb webPage, int attempts, SafeThreadSleep threadSleep) {
         this.webPage = webPage;
         this.attempts = attempts;
+        this.threadSleep = threadSleep;
     }
 
     @Override
@@ -36,8 +42,8 @@ public final class RetriedWebPage implements JsoupWeb {
                 throw new IOException("Error in getting parsedHTML", ex);
             }
             System.out.println("Error! Retrying to get parsed HTML. Attempts left: " + this.attempts);
-            new SafeThreadSleep(5000).sleep();
-            return new RetriedWebPage(this.webPage, this.attempts - 1)
+            threadSleep.sleep();
+            return new RetriedWebPage(this.webPage, this.attempts - 1, this.threadSleep)
                     .parsedHTML();
         }
     }
