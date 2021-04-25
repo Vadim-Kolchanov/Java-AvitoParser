580bd3d5d21cf5f362cd5b1cc0e1e1e15fcb2acb
Vadim Kolchanov <ikolchanov14@gmail.com>
2021-04-25 23:15:30 +0500

    JUnit is adding

---
+++ src/test/Version2/ru/Avito/Parser/WebPage/JWebCachedTest.java
@@ -0,0 +1,15 @@
+package Version2.ru.Avito.Parser.WebPage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author Kolchanov Vadim
+ */
+class JWebCachedTest {
+
+    @Test
+    void parsedHTML() {
+    }
+}
